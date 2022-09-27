4afa4898d3a75c55a355c3a0d8f0f855927b7aaa
Aung Khant Zaw <akhantz250@gmail.com>
2022-09-27 19:11:10 +0800

    Home page

---
+++ index.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./src/stylesheets/style.css">
+    <title>Home</title>
+</head>
+
+<body>
+    <header class="header">
+        <p id="site-name">PRIMAVERA</p>
+        <nav class="nav-bar">
+            <ul>
+                <li>Menu</li>
+                <li>About</li>
+                <li>Contact</li>
+                <li>Orders</li>
+                <li>
+                    <div id="cart-button">
+                        <img id="shopping-cart-icon" src="./src/img/shopping_cart_FILL1_wght400_GRAD0_opsz24.svg" alt="">
+                        <p>0 items</p>
+                    </div>
+                </li>
+            </ul>
+        </nav>
+    </header>
+    <main>
+        <section class="banner">
+            <h1>Your culinary adventure awaits</h1>
+            <button>See our menu</button>
+        </section>
+        <section class="weekly-specials">
+            <h1>Our weekly specials</h1>
+            <div class="item-container">
+                <div class="item">
+                    <img src="./src/img/fooditems/food1.png" alt="">
+                    <div class="item-des">
+                        <p class="item-name">Item 1</p>
+                        <p class="item-subtext">Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi</p>
+                    </div>
+                </div>
+                <div class="item">
+                    <img src="./src/img/fooditems/food2.png" alt="">
+                    <div class="item-des">
+                        <p class="item-name">Item 2</p>
+                        <p class="item-subtext">Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi</p>
+                    </div>
+                </div>
+                <div class="item">
+                    <img src="./src/img/fooditems/food3.png" alt="">
+                    <div class="item-des">
+                        <p class="item-name">Item 3</p>
+                        <p class="item-subtext">Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi</p>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </main>
+    <footer>
+        Project for IE4717 by Zaw and Zion
+    </footer>
+</body>
+
+</html>
