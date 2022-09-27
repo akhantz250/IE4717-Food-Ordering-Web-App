3b4cc5026d66be1a48925f156f53647aee3d7ec2
Aung Khant Zaw <akhantz250@gmail.com>
2022-09-27 19:24:57 +0800

    added header php file

---
+++ inc/header.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./src/stylesheets/style.css">
+    <title>Primavera</title>
+</head>
+
+<body>
+    <header class="header">
+        <a href="index.php"><p id="site-name">PRIMAVERA</p></a>
+        <nav class="nav-bar">
+            <ul>
+                <li><a href="menu.php">Menu</a></li>
+                <li><a href="">About</a></li>
+                <li><a href="">Contacts</a></li>
+                <li><a href="">Orders</a></li>
+                <li>
+                    <div id="cart-button">
+                        <img id="shopping-cart-icon" src="./src/img/shopping_cart_FILL1_wght400_GRAD0_opsz24.svg" alt="">
+                        <p>0 items</p>
+                    </div>
+                </li>
+            </ul>
+        </nav>
+    </header>
